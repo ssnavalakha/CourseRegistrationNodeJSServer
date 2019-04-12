8414e70a80e638d95f4ca9778a95b93b783a279c
Sanket <ssnavalakha@gmail.com>
2019-04-11 23:19:04 -0400

    deploy mbdb

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ module.exports = function () {
     const mongoose = require('mongoose');
     const databaseName = 'white-board';
     var   connectionString =
-        process.env.MONGODB_URI;
-    connectionString += databaseName;
+        process.env.MONGOLAB_URI;
+    //connectionString += databaseName;
     mongoose.connect(connectionString);
 };
